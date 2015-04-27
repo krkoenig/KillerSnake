0b4614d8cb1f7325cccabfaf205be66ca4188abc
krkoenig <krkoenig@uci.edu>
2015-04-27 01:35:13 -0700

    Moved loginScript to custom Login
    TODO: Make server into a unity project
    Restructure repo into 2 unity project (client and server)
    Move database stuff to server

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using scMessage;
 
 // For password security
 using System.Security.Cryptography;
@@ -13,69 +17,179 @@ using System.Data;
 public class Login : MonoBehaviour
 {
 
-		private string username;
-		private string password;
+	private string username;
+	private string password;
 
-		// Use this for initialization
-		void Start ()
-		{
-				username = "";
-				password = "";
+	private int
+		sPort = 3000, // server port
+		pfrPort = 2999; // policy file request port
 
-		}
+	private Socket
+		cSock; // client socket
 	
-		// Update is called once per frame
-		void Update ()
-		{
-	
+	private string
+		ipAddress = "127.0.0.1"; // server ip address
+
+	public bool
+		connectedToServer = false;
+
+	private List<message>
+		incMessages = new List<message> ();
+
+	private static Login instance;
+	public static Login Instance {
+		get {
+			return instance;
 		}
+	}
 
-		void OnGUI ()
-		{
-				float centerX = Screen.width / 2;
-				float centerY = Screen.height / 2;
+	void Awake ()
+	{
+		instance = this;
+	}
 
-				// Make a background box
-				GUI.Box (new Rect (centerX - 150, centerY - 100, 300, 200), "Login");
+	// Use this for initialization
+	void Start ()
+	{
+		username = "";
+		password = "";
+		connect ();
 
-				GUI.Box (new Rect (centerX - 125, centerY - 15, 100, 25), "Username:");
-				username = GUI.TextField (new Rect (centerX - 25, centerY - 15, 150, 25), username, 20);
-				GUI.Box (new Rect (centerX - 125, centerY + 15, 100, 25), "Password:");
-				password = GUI.PasswordField (new Rect (centerX - 25, centerY + 15, 150, 25), password, '*', 20);
+	}
 
-				if (GUI.Button (new Rect (centerX - 125, centerY + 60, 100, 25), "Login") && !username.Equals ("") && !password.Equals ("")) {
-						checkLoginWithDatabase ();
-				}
-				if (GUI.Button (new Rect (centerX + 25, centerY + 60, 100, 25), "Register")) {
-						// Load the register scene			
-						Application.LoadLevel (1);
-				}
+	void OnGUI ()
+	{
+		float centerX = Screen.width / 2;
+		float centerY = Screen.height / 2;
+
+		// Make a background box
+		GUI.Box (new Rect (centerX - 150, centerY - 100, 300, 200), "Login");
+
+		GUI.Box (new Rect (centerX - 125, centerY - 15, 100, 25), "Username:");
+		username = GUI.TextField (new Rect (centerX - 25, centerY - 15, 150, 25), username, 20);
+		GUI.Box (new Rect (centerX - 125, centerY + 15, 100, 25), "Password:");
+		password = GUI.PasswordField (new Rect (centerX - 25, centerY + 15, 150, 25), password, '*', 20);
+
+		if (GUI.Button (new Rect (centerX - 125, centerY + 60, 100, 25), "Login") && !username.Equals ("") && !password.Equals ("")) {
+			checkLoginWithDatabase ();
 		}
+		if (GUI.Button (new Rect (centerX + 25, centerY + 60, 100, 25), "Register")) {
+			// Load the register scene			
+			Application.LoadLevel (1);
+		}
+	}
 
-		private void checkLoginWithDatabase ()
-		{
-				SqliteConnection myConnection = new SqliteConnection ();
-				myConnection.ConnectionString = "URI=file:" + Application.dataPath + "/killer_snake.db";
-				myConnection.Open ();
+	private void checkLoginWithDatabase ()
+	{
+		SqliteConnection myConnection = new SqliteConnection ();
+		myConnection.ConnectionString = "URI=file:" + Application.dataPath + "/killer_snake.db";
+		myConnection.Open ();
 
-				string query = "SELECT * FROM users WHERE username = '" + username.ToUpper () + "';";
-				SqliteCommand cmd = new SqliteCommand (query, myConnection);
-				SqliteDataReader rdr = cmd.ExecuteReader ();
+		string query = "SELECT * FROM users WHERE username = '" + username.ToUpper () + "';";
+		SqliteCommand cmd = new SqliteCommand (query, myConnection);
+		SqliteDataReader rdr = cmd.ExecuteReader ();
 
-				string dbHash = "";
-				string salt = "";
-				if (rdr.Read ()) {
-						dbHash = rdr.GetString (1);
-						salt = rdr.GetString (2);
-				}
+		string dbHash = "";
+		string salt = "";
+		if (rdr.Read ()) {
+			dbHash = rdr.GetString (1);
+			salt = rdr.GetString (2);
+		}
 				
 
 
-				if (dbHash.Equals (password)) {
-						Application.LoadLevel (2);
-				}
-
-				rdr.Close ();
-				myConnection.Close ();
+		if (dbHash.Equals (password)) {
+			Application.LoadLevel (2);
 		}
+
+		rdr.Close ();
+		myConnection.Close ();
+	}
+
+	private void connect ()
+	{
+		try {
+			// get policy if we are on the web or in editor
+			if ((Application.platform == RuntimePlatform.WindowsWebPlayer) || (Application.platform == RuntimePlatform.WindowsEditor)) {
+				Security.PrefetchSocketPolicy (ipAddress, pfrPort);
+			}
+			
+			cSock = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			cSock.Connect (new IPEndPoint (IPAddress.Parse (ipAddress), sPort));
+			clientConnection gsCon = new clientConnection (cSock);
+		} catch {
+			Debug.Log ("Unable to connect to server.");
+		}
+	}
+
+	public void onConnect ()
+	{
+		connectedToServer = true;
+		
+		// test the connection
+		message testMessage = new message ("LOGIN");
+		SendServerMessage (testMessage);
+	}
+	
+	private void OnApplicationQuit ()
+	{
+		try {
+			cSock.Close ();
+		} catch {
+		}
+	}
+	
+	public void addServerMessageToQue (message msg)
+	{
+		incMessages.Add (msg);
+	}
+	
+	void Update ()
+	{
+		if (incMessages.Count > 0) {
+			doMessages ();
+		}
+	}
+	
+	private void doMessages ()
+	{
+		// do messages
+		List<message> completedMessages = new List<message> ();
+		for (int i = 0; i < incMessages.Count; i++) {
+			try {
+				handleData (incMessages [i]);
+				completedMessages.Add (incMessages [i]);
+			} catch {
+			}
+		}
+		
+		// delete completed messages
+		for (int i = 0; i < completedMessages.Count; i++) {
+			try {
+				incMessages.Remove (completedMessages [i]);
+			} catch {
+			}
+		}
+	}
+	
+	private void handleData (message mess)
+	{
+		Debug.Log ("The server sent a message: " + mess.messageText);
+	}
+	
+	public void SendServerMessage (message mes)
+	{
+		if (connectedToServer) {
+			try {
+				// convert message into a byte array, wrap the message with framing
+				byte[] messageObject = conversionTools.convertObjectToBytes (mes);
+				byte[] readyMessage = conversionTools.wrapMessage (messageObject);
+				
+				// send completed message
+				cSock.Send (readyMessage);
+			} catch {
+				Debug.Log ("There was an error sending server message " + mes.messageText);
+			}
+		}
+	}
 }
--- Assets/Scripts/scConnection/clientConnection.cs
+++ Assets/Scripts/scConnection/clientConnection.cs
@@ -10,67 +10,59 @@ using scMessage;
 
 public class clientConnection
 {
-    public Socket sSock;
-    private int MAX_INC_DATA = 512000;
+	public Socket sSock;
+	private int MAX_INC_DATA = 512000;
 
-    public clientConnection(Socket s)
-    {
-        sSock = s;
-        ThreadPool.QueueUserWorkItem(new WaitCallback(HandleConnection));
-    }
+	public clientConnection (Socket s)
+	{
+		sSock = s;
+		ThreadPool.QueueUserWorkItem (new WaitCallback (HandleConnection));
+	}
 
-    public void HandleConnection(object state)
-    {
-        Debug.Log("Connected to server.");
-        loginScript.Instance.onConnect();
+	public void HandleConnection (object state)
+	{
+		Debug.Log ("Connected to server.");
+		Login.Instance.onConnect ();
 
-        try
-        {
-            while (sSock.Connected)
-            {
-                byte[] sizeinfo = new byte[4];
+		try {
+			while (sSock.Connected) {
+				byte[] sizeinfo = new byte[4];
 
-                int bytesRead = 0, currentread = 0;
+				int bytesRead = 0, currentread = 0;
 
-                currentread = bytesRead = sSock.Receive(sizeinfo);
+				currentread = bytesRead = sSock.Receive (sizeinfo);
 
-                while (bytesRead < sizeinfo.Length && currentread > 0)
-                {
-                    currentread = sSock.Receive(sizeinfo, bytesRead, sizeinfo.Length - bytesRead, SocketFlags.None);
-                    bytesRead += currentread;
-                }
+				while (bytesRead < sizeinfo.Length && currentread > 0) {
+					currentread = sSock.Receive (sizeinfo, bytesRead, sizeinfo.Length - bytesRead, SocketFlags.None);
+					bytesRead += currentread;
+				}
 
-                int messagesize = BitConverter.ToInt32(sizeinfo, 0);
-                byte[] message = new byte[messagesize];
+				int messagesize = BitConverter.ToInt32 (sizeinfo, 0);
+				byte[] message = new byte[messagesize];
 
-                bytesRead = 0;
-                currentread = bytesRead = sSock.Receive(message, bytesRead, message.Length - bytesRead, SocketFlags.None);
+				bytesRead = 0;
+				currentread = bytesRead = sSock.Receive (message, bytesRead, message.Length - bytesRead, SocketFlags.None);
 
-                while (bytesRead < messagesize && currentread > 0)
-                {
-                    currentread = sSock.Receive(message, bytesRead, message.Length - bytesRead, SocketFlags.None);
-                    bytesRead += currentread;
-                }
+				while (bytesRead < messagesize && currentread > 0) {
+					currentread = sSock.Receive (message, bytesRead, message.Length - bytesRead, SocketFlags.None);
+					bytesRead += currentread;
+				}
 
-                try
-                {
-                    message incObject = (message)conversionTools.convertBytesToObject(message);
+				try {
+					message incObject = (message)conversionTools.convertBytesToObject (message);
 
-                    if (incObject != null)
-                    {
-                        loginScript.Instance.addServerMessageToQue(incObject);
-                    }
-                }
-                catch (Exception er)
-                {
-                    Debug.Log("There was an error trying to get data: " + er.ToString());
-                }
-            }
-        }
-        catch { }
+					if (incObject != null) {
+						Login.Instance.addServerMessageToQue (incObject);
+					}
+				} catch (Exception er) {
+					Debug.Log ("There was an error trying to get data: " + er.ToString ());
+				}
+			}
+		} catch {
+		}
 
-        Debug.Log("Disconnected from server.");
-        loginScript.Instance.connectedToServer = false;
-        sSock.Close();
-    }
+		Debug.Log ("Disconnected from server.");
+		Login.Instance.connectedToServer = false;
+		sSock.Close ();
+	}
 }

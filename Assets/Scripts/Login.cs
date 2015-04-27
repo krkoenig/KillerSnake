a1f07ed9685e7ebc926942aa5aed51b847892728
krkoenig <krkoenig@uci.edu>
2015-04-26 23:46:16 -0700

    Login and Registration Working

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -7,7 +7,8 @@ using System.Security.Cryptography;
 using System.Text;
 
 // For connecting to the database
-using MySql.Data.MySqlClient;
+using Mono.Data.SqliteClient;
+using System.Data;
 
 public class Login : MonoBehaviour
 {
@@ -15,20 +16,12 @@ public class Login : MonoBehaviour
 		private string username;
 		private string password;
 
-		// Security parameters
-		HashAlgorithm HashProvider;
-		private const int SALT_LENGTH = 4;
-
-		// Database information
-
-
 		// Use this for initialization
 		void Start ()
 		{
-				HashProvider = new SHA256Managed ();
-
 				username = "";
 				password = "";
+
 		}
 	
 		// Update is called once per frame
@@ -45,7 +38,6 @@ public class Login : MonoBehaviour
 				// Make a background box
 				GUI.Box (new Rect (centerX - 150, centerY - 100, 300, 200), "Login");
 
-
 				GUI.Box (new Rect (centerX - 125, centerY - 15, 100, 25), "Username:");
 				username = GUI.TextField (new Rect (centerX - 25, centerY - 15, 150, 25), username, 20);
 				GUI.Box (new Rect (centerX - 125, centerY + 15, 100, 25), "Password:");
@@ -55,18 +47,35 @@ public class Login : MonoBehaviour
 						checkLoginWithDatabase ();
 				}
 				if (GUI.Button (new Rect (centerX + 25, centerY + 60, 100, 25), "Register")) {
-						// login...
+						// Load the register scene			
+						Application.LoadLevel (1);
 				}
 		}
 
 		private void checkLoginWithDatabase ()
 		{
-				MySqlConnection myConnection = new MySqlConnection ();
-				myConnection.ConnectionString = "Server=localhost;Database=killer_snake;port=3306;User ID=root;";
+				SqliteConnection myConnection = new SqliteConnection ();
+				myConnection.ConnectionString = "URI=file:" + Application.dataPath + "/killer_snake.db";
 				myConnection.Open ();
 
-				Debug.Log (myConnection.Database);
+				string query = "SELECT * FROM users WHERE username = '" + username.ToUpper () + "';";
+				SqliteCommand cmd = new SqliteCommand (query, myConnection);
+				SqliteDataReader rdr = cmd.ExecuteReader ();
 
+				string dbHash = "";
+				string salt = "";
+				if (rdr.Read ()) {
+						dbHash = rdr.GetString (1);
+						salt = rdr.GetString (2);
+				}
+				
+
+
+				if (dbHash.Equals (password)) {
+						Application.LoadLevel (2);
+				}
+
+				rdr.Close ();
 				myConnection.Close ();
 		}
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,123 +5,123 @@ using System.Collections.Generic;
 public class Snake : MonoBehaviour
 {
 
-	// The parts of a snake are made up of prefab parts
-	// Last() is the tail
-	private List<Transform> segments;
+		// The parts of a snake are made up of prefab parts
+		// Last() is the tail
+		private List<Transform> segments;
 
-	// Prefab for the body parts
-	public GameObject body;
-	public GameObject tail;
+		// Prefab for the body parts
+		public GameObject body;
+		public GameObject tail;
 
-	// The speed of the snake in ms
-	// intially 0.1
-	private float speed;
+		// The speed of the snake in ms
+		// intially 0.1
+		private float speed;
 
-	// Rotation direction
-	private Quaternion dir;
+		// Rotation direction
+		private Quaternion dir;
 
-	// Helps simulate responsiveness
-	private Quaternion lastRotation;
+		// Helps simulate responsiveness
+		private Quaternion lastRotation;
 
-	// Bools for food eating
-	private bool apple;
-	private bool onion;
-	private bool mouse;
+		// Bools for food eating
+		private bool apple;
+		private bool onion;
+		private bool mouse;
 
-	// Use this for initialization
-	void Start ()
-	{
-		dir = Quaternion.identity;
+		// Use this for initialization
+		void Start ()
+		{
+				dir = Quaternion.identity;
 
-		// Sets the snake to start in the center
-		// TODO: Once multiplayer, the player number will matter
-		transform.position = new Vector3 (0, 0);
-		transform.rotation = dir;
+				// Sets the snake to start in the center
+				// TODO: Once multiplayer, the player number will matter
+				transform.position = new Vector3 (0, 0);
+				transform.rotation = dir;
 
-		segments = new List<Transform> ();
+				segments = new List<Transform> ();
 
-		calcSpeed ();
-		// Start with one part
-		InvokeRepeating ("grow", 1.0f, 0.5f);
+				calcSpeed ();
+				// Start with one part
+				InvokeRepeating ("grow", 1.0f, 0.5f);
 
-		move ();
-	}
+				move ();
+		}
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		input ();
-	}
-
-	void OnTriggerEnter2D (Collider2D coll)
-	{
-		Application.LoadLevel (0);
-	}
-	
-	private void move ()
-	{
-		if (segments.Count > 0) {
-			for (int i = segments.Count - 1; i > 0; i--) {
-				segments [i].position = segments [i - 1].position;
-				segments [i].rotation = segments [i - 1].rotation;
-			}
-
-			segments [0].position = transform.position;
-			segments [0].rotation = transform.rotation;
+		// Update is called once per frame
+		void Update ()
+		{
+				input ();
 		}
 
-		lastRotation = transform.rotation;
-		transform.Translate (Vector2.right);
-	
-		Invoke ("move", speed);
-	}
-
-	public void grow ()
-	{
-		Vector3 pos = transform.position;
-		Quaternion rot = transform.rotation;
-		if (segments.Count > 0) {
-			pos = segments [segments.Count - 1].transform.position;
-			rot = segments [segments.Count - 1].transform.rotation;
+		void OnTriggerEnter2D (Collider2D coll)
+		{
+				Application.LoadLevel (2);
 		}
+	
+		private void move ()
+		{
+				if (segments.Count > 0) {
+						for (int i = segments.Count - 1; i > 0; i--) {
+								segments [i].position = segments [i - 1].position;
+								segments [i].rotation = segments [i - 1].rotation;
+						}
+
+						segments [0].position = transform.position;
+						segments [0].rotation = transform.rotation;
+				}
+
+				lastRotation = transform.rotation;
+				transform.Translate (Vector2.right);
+	
+				Invoke ("move", speed);
+		}
+
+		public void grow ()
+		{
+				Vector3 pos = transform.position;
+				Quaternion rot = transform.rotation;
+				if (segments.Count > 0) {
+						pos = segments [segments.Count - 1].transform.position;
+						rot = segments [segments.Count - 1].transform.rotation;
+				}
 				
-		GameObject newSegment = (GameObject)Instantiate (body, pos - rot * new Vector3 (1, 0, 0), rot);
+				GameObject newSegment = (GameObject)Instantiate (body, pos - rot * new Vector3 (1, 0, 0), rot);
 
-		segments.Add (newSegment.transform);
+				segments.Add (newSegment.transform);
 
-		calcSpeed ();
-	}
+				calcSpeed ();
+		}
 
 
-	private void calcSpeed ()
-	{
-		speed = 0.1f + 0.005f * segments.Count;
+		private void calcSpeed ()
+		{
+				speed = 0.1f + 0.005f * segments.Count;
 		
-		if (speed > 0.20f) {
-			speed = 0.20f;
-		}
-	}
-
-	private void input ()
-	{
-		// Use the given input for compatability and easier customization
-		float horizontal = Input.GetAxisRaw ("Horizontal");
-		float vertical = Input.GetAxisRaw ("Vertical");
-
-		dir = transform.rotation;
-		// Snake can not move in the oppisite direction that it is moving
-		if (horizontal == -1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 0)) {			// LEFT
-			dir.eulerAngles = new Vector3 (0, 0, 180);
-		} else if (horizontal == 1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 180)) {	// RIGHT
-			dir.eulerAngles = new Vector3 (0, 0, 0);	
-		} else if (vertical == -1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 90)) {		// DOWN
-			dir.eulerAngles = new Vector3 (0, 0, 270);	
-		} else if (vertical == 1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 270)) {		// UP
-			dir.eulerAngles = new Vector3 (0, 0, 90);
+				if (speed > 0.20f) {
+						speed = 0.20f;
+				}
 		}
 
-		transform.rotation = dir;
-	}
+		private void input ()
+		{
+				// Use the given input for compatability and easier customization
+				float horizontal = Input.GetAxisRaw ("Horizontal");
+				float vertical = Input.GetAxisRaw ("Vertical");
+
+				dir = transform.rotation;
+				// Snake can not move in the oppisite direction that it is moving
+				if (horizontal == -1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 0)) {			// LEFT
+						dir.eulerAngles = new Vector3 (0, 0, 180);
+				} else if (horizontal == 1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 180)) {	// RIGHT
+						dir.eulerAngles = new Vector3 (0, 0, 0);	
+				} else if (vertical == -1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 90)) {		// DOWN
+						dir.eulerAngles = new Vector3 (0, 0, 270);	
+				} else if (vertical == 1 && !Mathf.Approximately (lastRotation.eulerAngles.z, 270)) {		// UP
+						dir.eulerAngles = new Vector3 (0, 0, 90);
+				}
+
+				transform.rotation = dir;
+		}
 		
 		
 }

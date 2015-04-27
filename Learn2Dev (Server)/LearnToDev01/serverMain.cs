fa522b3eccb4d686b9c181fd10e4dbcac78237b6
krkoenig <krkoenig@uci.edu>
2015-04-27 01:19:26 -0700

    Moved code into repo

---
+++ Learn2Dev (Server)/LearnToDev01/serverMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnToDev01
+{
+    class serverMain
+    {
+        public static bool keepAlive = true;
+
+        static void Main()
+        {
+            // start server
+            serverTCP startTCP = new serverTCP();
+
+            // keep alive
+            while (keepAlive)
+            {
+                if (Console.ReadLine().ToLower() == "exit")
+                {
+                    keepAlive = false;
+                }
+            }
+        }
+    }
+}
---
+++ Learn2Dev (Server)/scMessage/conversionTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace scMessage
+{
+    public class conversionTools
+    {
+        public static Object convertBytesToObject(byte[] b)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(b, 0, b.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            object obj = (object)binForm.Deserialize(memStream);
+            return obj;
+        }
+
+        public static byte[] convertObjectToBytes(object o)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            binForm.Serialize(memStream, o);
+            return memStream.ToArray();
+        }
+
+        public static byte[] wrapMessage(byte[] mes)
+        {
+            byte[] lengPre = BitConverter.GetBytes(mes.Length);
+            byte[] r = new byte[lengPre.Length + mes.Length];
+            lengPre.CopyTo(r, 0);
+            mes.CopyTo(r, lengPre.Length);
+            return r;
+        }
+    }
+}

4d8d7c4cab66c59dffe3e6e07ebe4c70040a0f95
Xiarong <xiarongx@uci.edu>
2015-05-09 16:01:50 -0700

    unity server

---
+++ KillerSnakeServer(unity)/Assets/serverMain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class serverMain : MonoBehaviour {
+
+	public static bool keepAlive = true;
+	
+	static void Main()
+	{
+		// start server
+		serverTCP startTCP = new serverTCP();
+		
+		// keep alive
+		while (keepAlive)
+		{
+			if (Console.ReadLine().ToLower() == "exit")
+			{
+				keepAlive = false;
+			}
+		}
+	}
+}
